3be2cf6e8b22b4150b54c5ec60e4ead69bc290d7
sunil7117 <sunilapp7117@gmail.com>
2023-10-11 00:05:04 +0530

    update function is activated for validate user

--- controller/updateController.js
+++ controller/updateController.js
@@ -1,3 +1,12 @@
-export const updateStatus=()=>{
+import UserModel from "../model/userModel.js"
 
+export const updateStatus=async(req,res)=>{
+    try {
+        const user=req.user
+        const update=req.body
+        const updateData=await UserModel.findOneAndUpdate({email:user.email},update,{new:true})
+        res.send(updateData)
+    } catch (error) {
+        res.send(error)
+    }
 }
