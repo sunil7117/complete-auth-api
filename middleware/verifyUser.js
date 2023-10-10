afbd174db97a2112012388cb288c83f8d4c38fea
sunil7117 <sunilapp7117@gmail.com>
2023-10-10 23:41:30 +0530

    jwt athentication is active

---
+++ middleware/verifyUser.js
@@ -0,0 +1,12 @@
+import  jwt  from "jsonwebtoken"
+export const verifyUser=(req,res,next)=>{
+    const KEY=process.env.ACCESSTOKEN
+    try {
+        const accesstoken=req.headers.authorization.split(' ')[1]
+        const decode=jwt.verify(accesstoken,KEY)
+        res.send(decode)
+        next()
+    } catch (error) {
+        return res.status(400).json(error.message)
+    }
+}
--- routeView/updateRoute.js
+++ routeView/updateRoute.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import { updateStatus } from '../controller/updateController.js'
+import { verifyUser } from '../middleware/verifyUser.js'
 const router=express.Router()
 
-router.put('/update',updateStatus)
+router.put('/update',verifyUser,updateStatus)
 export default router
