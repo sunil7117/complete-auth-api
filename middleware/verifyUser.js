c06c3605568522533fa36cb31a2e180d50b740de
sunil7117 <sunilapp7117@gmail.com>
2023-10-10 23:55:39 +0530

    send verified user details for modification[here i have change in req.body]

--- middleware/verifyUser.js
+++ middleware/verifyUser.js
@@ -4,7 +4,7 @@ export const verifyUser=(req,res,next)=>{
     try {
         const accesstoken=req.headers.authorization.split(' ')[1]
         const decode=jwt.verify(accesstoken,KEY)
-        req.user(decode)
+        req.user=decode
         next()
     } catch (error) {
         return res.status(400).json(error.message)
